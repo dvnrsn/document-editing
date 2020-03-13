d9d057db01f938373ddfd0409aaecbc20437479c
Devin Rasmussen <devin.rasmussen@canopytax.com>
2020-03-13 13:41:32 -0600

    implement sqlite

--- server/index.js
+++ server/index.js
@@ -1,6 +1,29 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const pino = require('express-pino-logger')();
+const sqlite3 = require('sqlite3').verbose();
+
+const path = require('path')
+const dbPath = path.resolve(__dirname, 'prizmdoc.db')
+
+// open database in memory
+let db = new sqlite3.Database(dbPath, sqlite3.OPEN_READWRITE | sqlite3.OPEN_CREATE, (err) => {
+  if (err) {
+    return console.error(err.message);
+  }
+  console.log('Connected to the ./db/prizmdoc.db SQlite database.');
+});
+
+db.serialize(() => {
+  db.run(`CREATE TABLE IF NOT EXISTS clients (id, name)`)
+    .run('CREATE TABLE IF NOT EXISTS documents (id, prizmDocId, title, clientId)')
+    .run('DELETE FROM clients')
+    .run(`INSERT INTO clients(id, name) VALUES(1, 'Melissa Harper');`)
+    .all('SELECT * FROM clients;', [], (err, rows) => {
+      console.log(rows)
+    })
+})
+db.close()
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
